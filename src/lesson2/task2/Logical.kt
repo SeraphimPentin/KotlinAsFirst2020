3da5fbf5399452cc845da7fc4e84856e7280325f
SerafimPentin <s5.pentin@yandex.ru>
2020-09-23 00:23:56 +0300

    исправил, что понял, как исправлять

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -20,8 +20,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = if (number / 1000 + number / 100 % 10 == number % 10 + number % 100 / 10)
-    true else false
+fun isNumberHappy(number: Int): Boolean = number / 1000 + number / 100 % 10 == number % 10 + number % 100 / 10
 
 /**
  * Простая (2 балла)
@@ -30,13 +29,9 @@ fun isNumberHappy(number: Int): Boolean = if (number / 1000 + number / 100 % 10 
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
-    return when {
-        (x1 == x2 || y1 == y2) -> true
-        (abs(x1 - x2) == abs(y1 - y2)) -> true
-        else -> false
-    }
-}
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = (x1 == x2 || y1 == y2 ||
+        (abs(x1 - x2) == abs(y1 - y2)))
+
 
 /**
  * Простая (2 балла)
@@ -61,7 +56,7 @@ fun daysInMonth(month: Int, year: Int): Int {
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = if (sqrt(sqr(x2 - x1) + sqr(y2 - y1)) + r1 <= r2) true else false
+): Boolean = sqrt(sqr(x2 - x1) + sqr(y2 - y1)) + r1 <= r2
 
 /**
  * Средняя (3 балла)
@@ -72,5 +67,5 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int
-): Boolean = if (r * s >= a * b || r * s >= b * c  || r * s >= a * c) true else false
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = r * s >= a * b || r * s >= b * c || r * s >= a * c
+
